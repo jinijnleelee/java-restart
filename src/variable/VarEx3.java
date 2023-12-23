81cd70d3eb6da8e5b1b8af0a6e40a6c4750c8b8f
jinijnleelee <jinijnleelee@gmail.com>
2023-12-23 23:54:39 +0900

    variable test

---
+++ src/variable/VarEx3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long num = 10000000000L;
+        boolean num2 = true;
+
+        System.out.println(num);
+        System.out.println(num2);
+    }
+}
