016ad03ceaaf9a20584e06c757367fa2ca8c6f29
LEE JINYOUNG <jinijnleelee@gmail.com>
2024-01-01 23:36:43 +0900

    OperationEx

---
+++ src/operator/OperationEx3.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class OperationEx3 {
+
+	public static void main(String[] args) {
+		int score = 0;
+		boolean op = 80 <= score && score  <= 100; 
+		System.out.println(op);
+	}
+
+}
