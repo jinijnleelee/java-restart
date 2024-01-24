43a4f17c6ddd95a0efb88a8b5a480fd5c842faa3
LEE JINYOUNG <jinijnleelee@gmail.com>
2024-01-24 22:56:10 +0900

    if else

---
+++ src/cond/If3.java
@@ -0,0 +1,30 @@
+package cond;
+
+public class If3 {
+
+	public static void main(String[] args) {
+		int age = 8;
+		//불필요한 중복검사
+		if(age <= 7) {
+			System.out.println("미취학");	
+		}
+		
+		if(age >= 8 && age <= 13) {
+			System.out.println("초등학생");
+			
+		}
+		if(age >= 14 && age <= 16) {
+			System.out.println("중학생");
+			
+		}
+		if(age >= 17 && age <= 19) {
+			System.out.println("성인");
+			
+		}
+		if(age >= 28) {
+
+		}
+		
+	}
+
+}
